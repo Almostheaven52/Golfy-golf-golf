e42aea5fe109f6777e29eb431e62cb54f84727df
Almostheaven52 <97544927+Almostheaven52@users.noreply.github.com>
2022-10-24 18:51:09 -0800

    Update gameScript.js

--- scripts/gameScript.js
+++ scripts/gameScript.js
@@ -3,12 +3,12 @@ import { games } from "./config.js";
 $(function () {
   
   $.each(games, function (index, item) {
-    var el = `<div class='card text-center mx-auto bg-grey' style="width: 200px"> \
-  <button class='cardOpen btn btn-outline-info'> ${item.title} </button> \
-   <div class='cardHide'> \
-       <br> \
-       <a href="${item.link}" target="_blank" class="link-info" >GAME LINK</a> \
-       <p class="text-pink">Create by: ${item.author} </p> \
+    var el = `<div class='card text-center mx-auto bg-grey' style="width: 200px">
+  <button class='cardOpen btn btn-outline-info'> ${item.title} </button>
+   <div class='cardHide'>
+       <br>
+       <a href="${item.link}" target="_blank" class="link-info" >GAME LINK</a>
+       <p class="text-pink">Create by: ${item.author} </p>
    </div>
  </div>`;
 
